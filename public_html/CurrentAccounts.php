ef40bf91dbe51d2579aeb05af23269cd97003a43
Joseph Rymer <josephrymer@tamu.edu>
2015-08-05 14:25:39 -0500

    Issue #7 fix

--- public_html/CurrentAccounts.php
+++ public_html/CurrentAccounts.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<?php session_start(); 
+<?php session_start();
 if(!empty($_SESSION['lgnuser'])){ ?>
     <html>
         <title> Create Accounts </title>
@@ -8,7 +8,7 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                   require 'PHP/Validation.php';
                   $db= new DataHandler($conn);
                   if(isset($_GET['accountid'])){
-                     $db->updateAccount($_POST,$_GET['accountid']); 
+                     $db->updateAccount($_POST,$_GET['accountid']);
                   }else{
                      $db->updateAccount($_POST,$_GET['accountid']);
                   }
@@ -22,7 +22,7 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                   }else{
                      $response = $db->getAccount();
                   }
-                                
+
                   ?>
          <link href="css/bootstrap.css" rel="stylesheet">
          <link href="css/bootstrap-theme.css" rel="stylesheet">
@@ -38,16 +38,17 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                        <a class="dropdown-toggle" data-toggle="dropdown"  role="button" aria-expanded="false"> Accounts <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
                          <li><a href="AccountCreation.php">Create Account</a></li>
-                         <li><a href="CurrentAccounts.php">Current Accounts</a></li> 
+                         <li><a href="CurrentAccounts.php">Current Accounts</a></li>
                         </ul>
                       </li>
+                        <?php   if($_SESSION['admin']==='1'){ ?>
                       <li role="presentation" class="dropdown">
                        <a class="dropdown-toggle" data-toggle="dropdown"  role="button" aria-expanded="false"> Users <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
                          <li><a href="UserCreation.php">Create User</a></li>
-                         <li><a href="CurrentUsers.php">Current Users</a></li> 
+                         <li><a href="CurrentUsers.php">Current Users</a></li>
                         </ul>
-                      </li>      
+                      </li>  <?php } ?>
                       <li role="presentation"><a href="PHP/Reader_Editor.php?logout=1">Logout</a></li>
                      </ul>
                     </div>
@@ -57,7 +58,7 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                 </div>
                 <div class="color-field">
                     <div class="row-fluid">
-                        <div class="span12 pull left breadcrumb"> 
+                        <div class="span12 pull left breadcrumb">
                             <div class="visible-xs">
                                 <div class="btn-group" style="float:right;">
                                  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-align-justify"></span></button>
@@ -65,13 +66,15 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                                    <li><a href="profile.php">Profile</a></li>
                                    <li><a href="AccountCreation.php">Create Account</a></li>
                                    <li><a href="CurrentAccounts.php">Current Accounts</a></li>
+                                     <?php   if($_SESSION['admin']==='1'){ ?>
                                    <li role="separator" class="divider"></li>
                                    <li><a href="UserCreation.php">Create User</a></li>
                                    <li><a href="CurrentUsers.php">Current Users</a></li>
+                                   <?php } ?>
                                    <li role="separator" class="divider"></li>
                                    <li><a href="PHP/Reader_Editor.php?logout=1">Logout</a></li>
                                  </ul>
-                                </div>  
+                                </div>
                             </div>
                             <form action="CurrentAccounts.php?accountsearch" method="POST" class="control form-inline" style="float:right;">
                              <input type="text" class="form-control" name="searchresult" placeholder="Search">
@@ -84,7 +87,7 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                              <a href="profile.php">
                               Profile
                              </a>
-                             >                         
+                             >
                              <a href="CurrentAccounts.php">
                               Current Accounts
                              </a>
@@ -110,7 +113,7 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                                 <th>Creator</th>
                               </thead>
                               <tbody>
-                                <?php   
+                                <?php
                                  while($row=mysqli_fetch_array($response,MYSQLI_ASSOC)){
                                 ?><tr>
                                    <td><?php echo $row['firstname']; ?></td>
@@ -164,17 +167,17 @@ if(!empty($_SESSION['lgnuser'])){ ?>
                         </div>
                     </div>
                 </div>
-            </div>        
+            </div>
             <script src='js/jquery-2.1.4.min.js'></script>
             <script src='js/bootstrap.js'></script>
-        </body> 
-        <div class="container text-center "> 
+        </body>
+        <div class="container text-center ">
             <footer>
-             <a title="Texas A&amp;M University" href="http://www.tamu.edu">Texas A&amp;M University</a>  
-             <a title="Employment" href="http://library.tamu.edu/about/employment/">Employment</a>  
-             <a title="Webmaster" href="http://library.tamu.edu/services/forms/contact-info.html">Webmaster</a>  
-             <a title="Legal" href="http://library.tamu.edu/about/general-information/legal-notices.html">Legal</a>  
-             <a title="Comments" href="http://guides.library.tamu.edu/AskTheLibraries">Comments</a>  
+             <a title="Texas A&amp;M University" href="http://www.tamu.edu">Texas A&amp;M University</a>
+             <a title="Employment" href="http://library.tamu.edu/about/employment/">Employment</a>
+             <a title="Webmaster" href="http://library.tamu.edu/services/forms/contact-info.html">Webmaster</a>
+             <a title="Legal" href="http://library.tamu.edu/about/general-information/legal-notices.html">Legal</a>
+             <a title="Comments" href="http://guides.library.tamu.edu/AskTheLibraries">Comments</a>
              <a title="979-845-3731" href="http://library.tamu.edu/about/phone/">979-845-3731</a>
              <a title="Site Map" href="http://library.tamu.edu/sitemap.html">Site Map</a>
              <a title="Accessibility" href="http://library.tamu.edu/accessibility/">Accessibility</a>
@@ -182,6 +185,6 @@ if(!empty($_SESSION['lgnuser'])){ ?>
         </div>
     </html>
 <?php }else{
-header("location:/AccountManagement/public_html/index.php?badlogin=1");   
-} 
+header("location:/AccountManagement/public_html/index.php?badlogin=1");
+}
 ?>
--- public_html/PHP/Reader_Editor.php
+++ public_html/PHP/Reader_Editor.php
@@ -11,13 +11,13 @@ include('dbConnect.php');
     }
     function login(){
          $sql="SELECT * FROM `users` where `username`='".$_POST['username']."' and `password`=PASSWORD('".$_POST['password']."')";
-         $result = $this->conn->query($sql);  
+         $result = $this->conn->query($sql);
          $count=mysqli_num_rows($result);
          $row= mysqli_fetch_assoc($result);
          if($row['admin']=='1'){
              $_SESSION['admin'] = '1';
          }
-         if($count == 1){ 
+         if($count == 1){
             $_SESSION['lgnuser']=$_POST['username'];
             $_SESSION['lgnuserinfo']=$row;
             header('location:../profile.php');
@@ -26,25 +26,25 @@ include('dbConnect.php');
          }
         }
     private function generateUsername(){
-        $username=substr($_POST['F_Name'],0,1).$_POST['L_Name'].rand(1000,9999);          
+        $username=substr($_POST['F_Name'],0,1).$_POST['L_Name'].rand(1000,9999);
         return $username;
-    }   
+    }
     private function generateExpirationDate($data){
         $startDate=time();
         if($_POST ['numdays']=='30'){
-            $expirationdate = strtotime('+1 month',$startDate); 
+            $expirationdate = strtotime('+1 month',$startDate);
         }else if($_POST['numdays']=='60'){
-            $expirationdate = strtotime('+2 month',$startDate); 
+            $expirationdate = strtotime('+2 month',$startDate);
         }else{
-            $expirationdate = strtotime('+3 month',$startDate); 
+            $expirationdate = strtotime('+3 month',$startDate);
         }
         return $expirationdate;
     }
     function createUser($data){
         $username=$this->generateUsername($data);
-        $sql="INSERT INTO `users`(`firstname`,`lastname`,`email`,`username`,`password`,`creator`,`lastupdate`)values('".$data["F_Name"]."','".$data["L_Name"]."','".$data["E_Mail"]."','".$username."',PASSWORD('".$data["Password"]."'),'".$_SESSION['lgnuser']."',(UNIX_TIMESTAMP(NOW())))";
+        $sql="INSERT INTO `users`(`firstname`,`lastname`,`email`,`username`,`password`,`creator`,`lastupdate`)values('".$data["F_Name"]."','".$data["L_Name"]."','".$data["E_Mail"]."','".$data["username"]."',PASSWORD('".$data["Password"]."'),'".$_SESSION['lgnuser']."',(UNIX_TIMESTAMP(NOW())))";
         $result= mysqli_query($this->conn, $sql);
-        header("location:profile.php?usersuccess=".$username."");
+        header("location:profile.php?usercreated");
     }
     function createAccount($data){
         $username=$this->generateUsername($data);
@@ -68,8 +68,8 @@ include('dbConnect.php');
     function getUser(){
             $sql="SELECT * FROM `users` ORDER BY `lastupdate` DESC";
             $result = mysqli_query($this->conn, $sql);
-            return $result; 
-        }  
+            return $result;
+        }
     function searchUser($usersearchdata){
         $sql="SELECT * FROM `users` where `username`='". $usersearchdata['searchresult']."' or `firstname`='". $usersearchdata['searchresult']."' or `lastname`='". $usersearchdata['searchresult']."'";
             $result = mysqli_query($this->conn, $sql);
@@ -81,11 +81,11 @@ include('dbConnect.php');
             }else if(isset($data['adminp'])){
                 $admin=false;
             }
-            $sql="UPDATE `users` SET  `firstname`='".$data['firstname']."',`lastname`='".$data['lastname']."',`email`='".$data['email']."',`username`='".$username."' ,`isadmin`='".$admin."' ,`lastupdate`=(UNIX_TIMESTAMP(NOW()))   WHERE `username`='".$username."'";        
-            $result=mysqli_query($this->conn,$sql); 
+            $sql="UPDATE `users` SET  `firstname`='".$data['firstname']."',`lastname`='".$data['lastname']."',`email`='".$data['email']."',`username`='".$username."' ,`isadmin`='".$admin."' ,`lastupdate`=(UNIX_TIMESTAMP(NOW()))   WHERE `username`='".$username."'";
+            $result=mysqli_query($this->conn,$sql);
      }
-     
-    function updateAccount($data,$accountid){         
+
+    function updateAccount($data,$accountid){
             if(isset($data['startdate'])){
              $formeddate=strtotime($data['startdate']);
              $sql="UPDATE `accounts` SET  `expireddate`='".$formeddate."' where `username`='".$accountid."'";
@@ -93,33 +93,32 @@ include('dbConnect.php');
             }else{
             $sql="UPDATE `accounts` SET  `username`='".$data['username']."' where `username`='".$accountid."'";
             $result=mysqli_query($this->conn,$sql);
-        } 
+        }
      }
-    function updateProfile($data){  
+    function updateProfile($data){
                 $sql="UPDATE `users` SET
                 `firstname` = '".$data['firstname']."',
                 `lastname` = '".$data['lastname']."',
-                `email` = '".$data['email']."',
-                `password` = PASSWORD('".$data['password']."') , lastupdate=(UNIX_TIMESTAMP(NOW())) WHERE username='".$_SESSION['lgnuser']."'";
+                `email` = '".$data['email']."', lastupdate=(UNIX_TIMESTAMP(NOW())) WHERE username='".$_SESSION['lgnuser']."'";
                 $result= mysqli_query($this->conn, $sql);
                 $sql="SELECT * FROM `users` WHERE `username`='".$_SESSION['lgnuser']."'";
                 $results=  mysqli_query($this->conn,$sql);
                 $row= mysqli_fetch_assoc($results);
                 $_SESSION['lgnuserinfo']=$row;
-                
+
             }
     function logout(){
         session_unset();
         header('location:../index.php');
     }
 }
-    
-    
-$run= new DataHandler($conn);   
+
+
+$run= new DataHandler($conn);
 if($_GET["attempt"]=="1"){
 $run->login();
 }
 if($_GET['logout']=='1'){
-$run->logout();    
+$run->logout();
 }
 ?>
